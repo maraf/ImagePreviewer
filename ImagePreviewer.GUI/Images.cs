42bc92da0bae798cca1e064c37266348c4333a0e
mara <maraf@users.noreply.github.com>
2011-01-14 22:37:35 +0000

    Initial, alfa 0.1

---
+++ ImagePreviewer.GUI/Images.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+
+namespace ImagePreviewer.GUI
+{
+    public class Images : ObservableCollection<Image>
+    {
+
+    }
+
+    public class Image : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private BitmapImage _bitmap;
+        private bool _checked;
+
+        public BitmapImage Bitmap
+        {
+            get
+            {
+                return _bitmap;
+            }
+            set
+            {
+                _bitmap = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Bitmap"));
+                }
+            }
+        }
+
+        public bool Checked
+        {
+            get
+            {
+                return _checked;
+            }
+            set
+            {
+                _checked = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Checked"));
+                }
+                
+            }
+        }
+    }
+}
---
+++ ImagePreviewer.GUI/SeriesInput.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImagePreviewer.GUI
+{
+    /// <summary>
+    /// Interaction logic for SeriesInput.xaml
+    /// </summary>
+    public partial class SeriesInput : Window
+    {
+        public event DetailButtonHandler LoadButtonClicked;
+
+        public SeriesInput()
+        {
+            InitializeComponent();
+
+            tbxUrl.Focus();
+        }
+
+        public void btnLoad_Click(Object sender, RoutedEventArgs e)
+        {
+            Fire(LoadButtonClicked, this, e);
+        }
+
+        public List<string> Get() {
+            List<string> images = new List<string>();
+            int from = Int32.Parse(tbxFrom.Text);
+            int to = Int32.Parse(tbxTo.Text);
+
+            for (int i = from; i <= to; i++)
+            {
+                images.Add(String.Format(tbxUrl.Text, i));
+            }
+
+            return images;
+        }
+
+        protected void Fire(Delegate dlg, params object[] pList)
+        {
+            if (dlg != null)
+            {
+                this.Dispatcher.BeginInvoke(dlg, pList);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tbxUrl.Text = tbxExample.Text;
+        }
+    }
+}
